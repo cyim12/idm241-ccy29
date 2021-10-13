ee98ccf656a500d427aae1bc08e4f4aa71d8c580
cyim12 <71360341+cyim12@users.noreply.github.com>
2021-10-13 03:14:00 -0400

    js and fixes

---
+++ alpha/build/main.js
@@ -0,0 +1,29 @@
+var build = document.getElementById("build");
+var fadedIn = false;
+var height,
+		half;
+
+window.addEventListener("resize", adjustHeightVars);
+window.addEventListener("scroll", fadeBox);
+const checkpoint = 300;
+
+function fadeBox () {	
+	var image=document.getElementsByClassName("image")[0]
+  const currentScroll = window.pageYOffset+600;
+  if (currentScroll >= checkpoint) {
+    image.animate({'opacity':1},1500);
+    setTimeout(function(){ image.style.opacity = 1; }, 400);
+  } 
+}
+
+ 
+// window.addEventListener("scroll", () => {
+  
+// });
+
+function adjustHeightVars() {
+	height = window.innerHeight;
+	half = height * 0.35;
+}
+
+adjustHeightVars();
